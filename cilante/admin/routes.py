d78a8e30f6031fa2dd4dfa396bcf27966d1b13d9
dbitz <dbielosp@gmail.com>
2025-10-27 11:07:02 -0500

    refactorizar el codigo para eliminar errores y codigo duplicado de cilante/admin

--- cilante/admin/routes.py
+++ cilante/admin/routes.py
@@ -1,21 +1,20 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required
 from cilante.models import User
+from . import admin
 
-admin_bp = Blueprint('admin', __name__)
-
-@admin_bp.route('/admin')
+@admin.route('/admin')
 @login_required
 def admin_dashboard():
     return render_template('admin/dashboard.html')
 
-@admin_bp.route('/admin/users')
+@admin.route('/admin/users')
 @login_required
 def manage_users():
     users = User.query.all()
     return render_template('admin/manage_users.html', users=users)
 
-@admin_bp.route('/admin/users/delete/<int:user_id>', methods=['POST'])
+@admin.route('/admin/users/delete/<int:user_id>', methods=['POST'])
 @login_required
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
