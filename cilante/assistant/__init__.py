a25b97acb53828af80ecc1be1056bb368a0ad1c4
dbitz <dbielosp@gmail.com>
2025-10-27 11:07:29 -0500

    refactorizar el codigo para eliminar errores y codigo duplicado para cilante/assistant

--- cilante/assistant/__init__.py
+++ cilante/assistant/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-assistant_bp = Blueprint('assistant', __name__)
+assistant = Blueprint('assistant', __name__)
 
 from . import routes
--- cilante/assistant/routes.py
+++ cilante/assistant/routes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template, request, jsonify
-from .ml_model import predict_assistant_response
+from . import assistant
+#from .ml_model import predict_assistant_response
 
-assistant_bp = Blueprint('assistant', __name__)
-
-@assistant_bp.route('/assistant', methods=['GET', 'POST'])
-def assistant():
+@assistant.route('/assistant', methods=['GET', 'POST'])
+def assistantfunc():
     if request.method == 'POST':
         user_input = request.form.get('user_input')
-        response = predict_assistant_response(user_input)
-        return jsonify({'response': response})
+        #response = predict_assistant_response(user_input)
+        #return jsonify({'response': response})
+        return jsonify({'response': 'Hola'})
     return render_template('assistant.html')
