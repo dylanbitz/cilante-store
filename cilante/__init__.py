cf5e16c483b3098564ccff82719e530667115258
dbitz <dbielosp@gmail.com>
2025-10-27 19:32:11 -0500

    añadir flask-migration a la funcion que crea la app, conectando la bd a la webapp

--- cilante/__init__.py
+++ cilante/__init__.py
@@ -3,14 +3,20 @@ from .auth import auth as auth_blueprint
 from .shop import shop as shop_blueprint
 from .admin import admin as admin_blueprint
 from .assistant import assistant as assistant_blueprint
+from config import Config
+from .models import db
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
 
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    
     # Register blueprints
+    app.register_blueprint(shop_blueprint, url_prefix='/')
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    app.register_blueprint(shop_blueprint, url_prefix='/shop')
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
     app.register_blueprint(assistant_blueprint, url_prefix='/assistant')
 
