0ef39a7d3c926843de4abcbae3c54f578179abf3
dbitz <dbielosp@gmail.com>
2025-10-27 20:46:30 -0500

    eliminar funciones y modulos que no se utilizan en routes de shop

--- cilante/shop/routes.py
+++ cilante/shop/routes.py
@@ -1,21 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from .forms import ContactForm
+from flask import render_template
 from . import shop
 
 @shop.route('/')
 def index():
     return render_template('index.html')
-
-@shop.route('/product')
-def product():
-    # Aquí se puede agregar la lógica para obtener información del producto
-    return render_template('product.html')
-
-@shop.route('/contact', methods=['GET', 'POST'])
-def contact():
-    form = ContactForm()
-    if form.validate_on_submit():
-        # Aquí se puede agregar la lógica para enviar el formulario
-        flash('Mensaje enviado con éxito', 'success')
-        return redirect(url_for('shop.contact'))
-    return render_template('contact.html', form=form)
