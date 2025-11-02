26852d0cfbef0cce3e2ac0e311329e7e4913e875
dbitz <dbielosp@gmail.com>
2025-11-01 23:02:27 -0500

    refactorizar strings a español en auth/forms

--- cilante/auth/forms.py
+++ cilante/auth/forms.py
@@ -4,23 +4,23 @@ from wtforms.validators import DataRequired, Email, EqualTo
 from cilante.models import Usuarios
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Nombre', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar Contraseña', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrarse')
 
     def validate_username(self, username):
         user = Usuarios.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('Ese nombre de usuario ya está en uso. Por favor elige uno diferente.')
 
     def validate_email(self, email):
         user = Usuarios.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is already registered. Please choose a different one.')
+            raise ValidationError('Ese correo electrónico ya está registrado. Por favor elige uno diferente.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    submit = SubmitField('Ingresar')
